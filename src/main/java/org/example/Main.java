1cd4bd21b5d3b0fe54ceefa7fb57b566bca13c99
El-Duckster <elena.fredenbrink@gmail.com>
2026-08-19 15:17:48 +0200

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    void main() {
+        String test="test1";
+        IO.println(test);
+    }
+}
