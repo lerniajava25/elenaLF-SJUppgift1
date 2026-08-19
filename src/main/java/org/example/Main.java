7b7960eda7ecf098751b88f4cd0b2c7069c486c2
El-Duckster <elena.fredenbrink@gmail.com>
2026-08-19 20:15:38 +0200

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     void main() {
-        String test="test1";
+        String test="test2";
         IO.println(test);
     }
 }
