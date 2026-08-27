a0b6b010e6d6471bf113fcb51f43e7cc8a5f51a4
El-Duckster <elena.fredenbrink@gmail.com>
2026-08-27 13:28:25 +0200

    feature: add charging window records to register the cheapest price for 4 hour in a row

---
+++ src/main/java/elprices/domain/CharginWindow.java
@@ -0,0 +1,20 @@
+package elprices.domain;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+// start and end of the cheapest 4 hours  window with the cheapest time to charge object
+
+public record CharginWindow(
+        OffsetDateTime start,
+        OffsetDateTime end,
+        double averageOre,
+        List<HourlyPrice> hours
+) {
+
+    //  add a constructor to have an immutable copy of this record i am going to copy it
+    public CharginWindow {
+        hours = List.copyOf(hours);
+    }
+}
+
