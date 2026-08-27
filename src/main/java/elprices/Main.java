7c26169dbc4e97718159fbc334482c5cebc79511
El-Duckster <elena.fredenbrink@gmail.com>
2026-08-27 22:21:11 +0200

    feature: improved  ElPriceApiClient and added extra cases for response status code.

--- src/main/java/elprices/Main.java
+++ src/main/java/elprices/Main.java
@@ -1,12 +1,17 @@
 package elprices;
 
-import elprices.infrastructure.ElPriceService;
+import elprices.infrastructure.ElPriceApiClient;
+import  java.time.LocalDate;
+import java.net.http.HttpClient;
+
+import static elprices.domain.ElectricityArea.SE1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     void main() {
-    ElPriceService service = new ElPriceService();
-        service.fetchPrices();
+        HttpClient httpClient = HttpClient.newHttpClient();
+        ElPriceApiClient service = new ElPriceApiClient(httpClient);
+       IO.println(service.fetchJson(SE1,  LocalDate.now()));
     }
 }
